e9e83efea34cbe3296aa0694472fbb06142ca48d
bamelegari <bamelegari@alaska.edu>
2016-09-14 11:53:37 -0800

    moo

--- iptablesRules.sh
+++ iptablesRules.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-iptables -F;
-ip6tables -F;
-iptables -X;
-ip6tables -X;
-iptables -P FORWARD DROP;
-ip6tables -P FORWARD DROP;
-iptables -P OUTPUT DROP;
-ip6tables -P OUTPUT DROP;
-iptables -P INPUT DROP;
-ip6tables -P INPUT DROP;
-iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT;
-ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT;
-iptables --append OUTPUT -p udp -s 0.0.0.0/0 --sport 68 -d 10.250.100.12/32 --dport 67 --jump ACCEPT;
-iptables --append OUTPUT -p udp -s 0.0.0.0/0 -d 8.8.8.8/32 --dport 53 --jump ACCEPT;
-iptables --append OUTPUT -p tcp -s 0.0.0.0/0 -d 0.0.0.0/0 --dport 22 --jump ACCEPT;
-iptables --append OUTPUT -p tcp -s 0.0.0.0/0 -d 0.0.0.0/0 --dport 80 --jump ACCEPT;
-iptables --append OUTPUT -p tcp -s 0.0.0.0/0 -d 0.0.0.0/0 --dport 443 --jump ACCEPT;
-iptables -N LOG_OUT;
-ip6tables -N LOG_OUT;
-iptables -N LOG_IN;
-ip6tables -N LOG_IN;
-iptables -A OUTPUT -j LOG_OUT;
-ip6tables -A OUTPUT -j LOG_OUT;
-iptables -A INPUT -j LOG_IN;
-ip6tables -A INPUT -j LOG_IN;
-iptables -A LOG_OUT -m limit --limit 2/min -j LOG --log-prefix "iptables-out: " --log-level 4;
-ip6tables -A LOG_OUT -m limit --limit 2/min -j LOG --log-prefix "ip6tables-out: " --log-level 4;
-iptables -A LOG_IN -m limit --limit 2/min -j LOG --log-prefix "iptables-in: " --log-level 4;
-ip6tables -A LOG_IN -m limit --limit 2/min -j LOG --log-prefix "ip6tables-in: " --log-level 4;
-iptables -A LOG_OUT -j DROP;
-ip6tables -A LOG_OUT -j DROP;
-iptables -A LOG_IN -j DROP;
-ip6tables -A LOG_IN -j DROP;
+iptables -F
+ip6tables -F
+iptables -X
+ip6tables -X
+iptables -P FORWARD DROP
+ip6tables -P FORWARD DROP
+iptables -P OUTPUT DROP
+ip6tables -P OUTPUT DROP
+iptables -P INPUT DROP
+ip6tables -P INPUT DROP
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables --append OUTPUT -p udp -s 0.0.0.0/0 --sport 68 -d 10.250.100.12/32 --dport 67 --jump ACCEPT
+iptables --append OUTPUT -p udp -s 0.0.0.0/0 -d 8.8.8.8/32 --dport 53 --jump ACCEPT
+iptables --append OUTPUT -p tcp -s 0.0.0.0/0 -d 0.0.0.0/0 --dport 22 --jump ACCEPT
+iptables --append OUTPUT -p tcp -s 0.0.0.0/0 -d 0.0.0.0/0 --dport 80 --jump ACCEPT
+iptables --append OUTPUT -p tcp -s 0.0.0.0/0 -d 0.0.0.0/0 --dport 443 --jump ACCEPT
+iptables -N LOG_OUT
+ip6tables -N LOG_OUT
+iptables -N LOG_IN
+ip6tables -N LOG_IN
+iptables -A OUTPUT -j LOG_OUT
+ip6tables -A OUTPUT -j LOG_OUT
+iptables -A INPUT -j LOG_IN
+ip6tables -A INPUT -j LOG_IN
+iptables -A LOG_OUT -m limit --limit 2/min -j LOG --log-prefix "iptables-out: " --log-level 4
+ip6tables -A LOG_OUT -m limit --limit 2/min -j LOG --log-prefix "ip6tables-out: " --log-level 4
+iptables -A LOG_IN -m limit --limit 2/min -j LOG --log-prefix "iptables-in: " --log-level 4
+ip6tables -A LOG_IN -m limit --limit 2/min -j LOG --log-prefix "ip6tables-in: " --log-level 4
+iptables -A LOG_OUT -j DROP
+ip6tables -A LOG_OUT -j DROP
+iptables -A LOG_IN -j DROP
+ip6tables -A LOG_IN -j DROP
